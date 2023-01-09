4d983163acfb73fe42eb023c89b0e11acd53c8d1
Nazmul Ahsan <nahsan527@gmail.com>
2023-01-09 15:34:11 +0600

    Main component has been modified. image added. Main compnent styling

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,13 +1,16 @@
-
 import React from "react"
+
 export default function Main(){
     return(
-        <div>
-            <h1>Main Components</h1>
-            <ul>
-                <li>It's time to learn React</li>
-                <li>Learning react is very interesting</li>
-            </ul>
-        </div>
+        <main>
+        <h1 className="main--title">Fun Facts About React</h1>
+        <ul className="main--facts">
+            <li>Was first Release in 2013</li>
+            <li>Was Originally created by Jordan walker</li>
+            <li>Has well over 100k stars on GitHub</li>
+            <li>Is Maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including Mobile apps</li>
+        </ul>
+    </main>
     )
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,10 @@
 import React from "react"
-export default function Navbar(){
-    return(
-        <div>
-            <h1>Navbar components</h1>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </div>
+export default function Navbar() {
+    return (
+       <nav>
+           <img src="./images/reactjs-icon.png" className="nav--icon" />
+           <h3 className="nav--logo_text">ReactFacts</h3>
+           <h4 className="nav--title">React Course-Project 1</h4>
+       </nav>
     )
 }
